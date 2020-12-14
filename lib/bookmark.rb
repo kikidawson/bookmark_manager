a022237e2beb4891354f38c2568b3493a5f00815
Will Dixon <dixon.will1@gmail.com>
2020-12-14 15:20:05 +0000

    viewing bookmakers in a nice list

---
+++ lib/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark 
+ 
+    def list
+        ["www.bbc.co.uk", "www.miniclip.com", "www.github.com"]
+    end
+
+end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,3 +4,13 @@ feature 'homepage' do
     expect(page).to have_content "Hello World"
   end
 end
+
+feature 'view the bookmarks' do
+  scenario 'show me the bookmarks' do 
+    visit '/bookmarks'
+
+    expect(page).to have_content "www.bbc.co.uk"
+    expect(page).to have_content "www.github.com"
+    expect(page).to have_content "www.miniclip.com"
+  end
+end
